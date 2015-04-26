faeb57fdbaf87d4abfe556bb2c2de18fec46088f
ZHOUGAO <zhougao81@gmail.com>
2015-04-25 17:34:34 -0700

    cachematrix.R submission

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,37 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Caching the Mean of a square Matrix (no singular)
 
-## Write a short comment describing this function
+
+## This function give a list of 4 functions, which are 'set','get'
+## 'setinverse' and 'getinverse'
+## for example, F<-makeCacheMatrix, then F$set('matrix'), to apply function$set
 
 makeCacheMatrix <- function(x = matrix()) {
-
+	m<-NULL
+	set<-function(y){
+		x<<-y
+		m<<-NULL
+	}
+	get <- function() x
+	setinverse <- function(matrixinverse) {m<<-matrixinverse}
+	getinverse <- function() m
+	list(set = set, get = get,
+		setinverse = setinverse,
+		getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## The second fuction first searches for the cached inverse of the Matrix
+## It will return a matrix that is the invese of 'x' either after calculation
+## Or it will print out "getting cached data" and give the inverse cached in environment
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+	m <- x$getinverse()
+	if(!is.null(m)) {
+		message("getting cached data")
+		return(m)
+	} 
+	data <-x$get()
+	m <- solve(data, ...)
+	x$setinverse(m)
+	m
 }
